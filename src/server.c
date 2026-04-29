972f8d93e4cb81bd38dae0dd585089313b7ff206
Alex Harrison <alexander.harrison.r@gmail.com>
2026-04-29 14:15:38 -0600

    change cert dir

--- src/server.c
+++ src/server.c
@@ -39,8 +39,8 @@ void ev_handler(struct mg_connection *c, int ev, void *ev_data) {
     }
     else if (ev == MG_EV_ACCEPT && c->is_tls) {
         struct mg_tls_opts opts = {
-            .cert = mg_file_read(&mg_fs_posix, "/etc/letsencrypt/live/albertamelee/fullchain.pem"),
-            .key = mg_file_read(&mg_fs_posix, "/etc/letsencrypt/live/albertamelee/privkey.pem"),
+            .cert = mg_file_read(&mg_fs_posix, "/etc/letsencrypt/live/albertamelee.com/fullchain.pem"),
+            .key = mg_file_read(&mg_fs_posix, "/etc/letsencrypt/live/albertamelee.com/privkey.pem"),
         };
         mg_tls_init(c, &opts);
     }
