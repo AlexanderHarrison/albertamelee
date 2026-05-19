5bf320b428242f1804830276cd105d02808c62a1
Alex Harrison <alexander.harrison.r@gmail.com>
2026-05-19 15:44:33 -0600

    maybe fix timer

--- services.sh
+++ services.sh
@@ -6,4 +6,6 @@ cp get_ab_tournaments.timer /etc/systemd/system/
 systemctl reenable server.service
 systemctl restart server.service
 systemctl reenable get_ab_tournaments.service
+systemctl reenable get_ab_tournaments.timer
 systemctl restart get_ab_tournaments.service
+systemctl restart get_ab_tournaments.timer
